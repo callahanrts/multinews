06f8f8dc3f2056bb458ab02b919b98fa7ee93a2a
Cody Callahan <CallahanRTS@gmail.com>
2013-08-23 23:42:53 -0700

    preview displays urls when items are clicked

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,10 @@
 window.onload = function() {
+	var window_height = $(window).height() - $('.menu-bar').height() - parseInt($('.menu-bar').css('padding-top'))* 2;
 	$(window).resize(function(){
-		$('.content').height($(window).height() - 54);
+		$('.content').height(window_height);
 	}).resize(); 
+
+	$('.news-list').resize(function(){
+		$('.preview').width($(window).width() - $('.news-list').width());
+	}).resize();
 }
