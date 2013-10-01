fcc97572fd666fa6ed462bba4ed661e54bcd43cb
Cody Callahan <CallahanRTS@gmail.com>
2013-10-01 13:45:16 -0700

    cleaned up some code

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Multinews
 =========
 
-Multinews is a news aggregator written in GO that allows you to storea all of your favorite news sources in a single location and easily switch between them. 
+Multinews is a news aggregator written in GO that allows you to storea all of your favorite news sources in a single location and easily switch between them. Although much of the application can be accomplished without the use of GO, it was a nice way to get to know the language. 
 
   - Reddit
     - Manage subreddits
@@ -17,7 +17,7 @@ git clone git@github.com:callahanrts/multinews.git multinews
 cd multinews
 ./multinews
 ```
-
+Once running, go to http://localhost:8080/
 
 
 License
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "log"
 	"io/ioutil"
 	"net/http"
 	"text/template"
@@ -14,24 +13,6 @@ type News struct {
 	JSONUrl string
 	NewsType string
 }
-
-// func viewHandler(w http.ResponseWriter, r *http.Request) {
-// 	news := []News{{"http://api.ihackernews.com/page","hackerNews"},{"http://www.reddit.com/r/programming/.json","reddit"}}
-
-//   //  // Loop through news articles
-// 	for _, source := range news {
-// 		articles, err := newsItems.Get(source.JSONUrl, source.NewsType)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-		
-// 		// Loop through items and print to web page
-// 		for _, item := range articles {
-// 			t, _ := template.ParseFiles("templates/items/" + source.NewsType + ".html")
-// 			t.Execute(w, item)
-// 		} // for items
-// 	} // for source types
-// } // function
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
  	t, _ := template.ParseFiles("templates/index.html")
